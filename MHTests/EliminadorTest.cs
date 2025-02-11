19e119d04115af9d905f9cf3eed8eb5a93b464b3
FedeArnaudo <federicoarnaudo.97@gmail.com>
2025-02-11 12:40:41 -0300

    Update Tests

---
+++ MHTests/EliminadorTest.cs
@@ -0,0 +1,131 @@
+﻿using MantenimientoHasar;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MHTests
+{
+    [TestClass]
+    public class EliminadorTest
+    {
+        private string ruta;
+        Eliminador eliminador;
+        List<string> carpetas;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            // Ruta donde se crea el directorio
+            ruta = @"C:\Syst\NewProject";
+
+            eliminador = new Eliminador();
+
+            carpetas = new List<string>
+            {
+                "Admi1",
+                "Admi2",
+                "Admi4",
+                "Admi8",
+                "Admi16"
+            };
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            if (Directory.Exists(@"C:\Syst"))
+            {
+                Directory.Delete(@"C:\Syst", true);
+            }
+        }
+
+        [TestMethod]
+        public void VerificarDirectorio_Existe()
+        {
+            // Arange
+
+            Directory.CreateDirectory(ruta);
+
+            // Act
+
+            bool condition = eliminador.VeridicarDirectorio(ruta);
+
+            // Assert
+
+            Assert.IsTrue(condition);
+        }
+
+        [TestMethod]
+        public void VerificarDirectorio_No_Existe()
+        {
+            // Arange
+
+            Directory.CreateDirectory(ruta);
+
+            // Act
+
+            bool condition = eliminador.VeridicarDirectorio(ruta);
+
+            // Assert
+
+            Assert.IsFalse(condition);
+        }
+
+        [TestMethod]
+        public void ObtenerSubCarpetas_ReturnOk()
+        {
+            // Arange
+            foreach (string carpeta in carpetas)
+            {
+                string subCarpeta = Path.Combine(ruta, carpeta);
+
+                Directory.CreateDirectory(subCarpeta);
+            }
+
+            List<string> value;
+
+            // Act
+            value = eliminador.ObtenerSubCarpetas(ruta);
+
+            // Assert
+            Assert.IsNotNull(value);
+        }
+
+        [TestMethod]
+        public void ObtenerSubCarpetas_Exc_ReturnNull()
+        {
+            // Arange
+            List<string> value;
+
+            // Act
+            value = eliminador.ObtenerSubCarpetas(ruta);
+
+            // Assert
+            Assert.IsNull(value);
+        }
+
+        [TestMethod]
+        public void ObtenerSubCarpetas_EsVacia_ReturnNull()
+        {
+            // Arange
+            foreach (string carpeta in carpetas)
+            {
+                string subCarpeta = Path.Combine(ruta, carpeta);
+
+                Directory.CreateDirectory(subCarpeta);
+            }
+
+            List<string> value;
+
+            // Act
+            value = eliminador.ObtenerSubCarpetas(ruta);
+
+            // Assert
+            Assert.IsNull(value);
+        }
+    }
+}
--- MantenimientoHasar/Eliminador.cs
+++ MantenimientoHasar/Eliminador.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,9 +22,76 @@ namespace MantenimientoHasar
             {
                 // TODO: Implemetnar la logica de borrado del archivo.
             }
+            catch (UnauthorizedAccessException e)
+            {
+                Log.Instance.WriteLog($"Error de acceso denegado al intentar eliminar el archivo. Excepción: {e.Message}", LogType.t_error);
+            }
+            catch (IOException e)
+            {
+                Log.Instance.WriteLog($"Error de entrada/salida al intentar eliminar el archivo. Excepción: {e.Message}", LogType.t_error);
+            }
             catch (Exception e)
             {
-                Log.Instance.WriteLog($"Error en el metodo EliminarArchivos. Excepción: {e.Message}", LogType.t_error);
+                Log.Instance.WriteLog($"Error en el método EliminarArchivos. Excepción: {e.Message}", LogType.t_error);
+            }
+        }
+
+        /// <summary>
+        /// Verifica que exista la ruta del proyecto.
+        /// </summary>
+        /// <param name="ruta"></param>
+        /// <returns></returns>
+        public bool VeridicarDirectorio(string ruta)
+        {
+            bool existe = false;
+
+            return existe;
+        }
+
+        /// <summary>
+        /// Devuelve los nombres de los subdirectorios (con sus rutas de acceso) del directorio especificado.
+        /// </summary>
+        /// <param name="rutaDirectorio"></param>
+        /// <returns></returns>
+        public List<string> ObtenerSubCarpetas(string ruta)
+        {
+            List<string> directorios = null;
+
+            try
+            {
+
+            }
+            catch (Exception e)
+            {
+                Log.Instance.WriteLog($"Error al obtener las subcarpetas. Excepción {e.Message}", LogType.t_error);
+            }
+
+            return directorios;
+        }
+
+        /// <summary>
+        /// Dentro de la carpeta, compara archivo por archivo.
+        /// Si hay coincidencia, se elimina dicho archivo.
+        /// </summary>
+        /// <param name="carpeta"></param>
+        /// <param name="archivo"></param>
+        public void Eliminar(string carpeta, string archivo)
+        {
+            try
+            {
+
+            }
+            catch (UnauthorizedAccessException e)
+            {
+                Log.Instance.WriteLog($"Error de acceso denegado al intentar eliminar el archivo en {carpeta}. Excepción: {e.Message}", LogType.t_error);
+            }
+            catch (IOException e)
+            {
+                Log.Instance.WriteLog($"Error de entrada/salida al intentar eliminar el archivo en {carpeta}. Excepción: {e.Message}", LogType.t_error);
+            }
+            catch (Exception e)
+            {
+                Log.Instance.WriteLog($"Error en el método Eliminar. Excepción: {e.Message}", LogType.t_error);
             }
         }
     }
